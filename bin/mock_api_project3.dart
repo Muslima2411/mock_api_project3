f5a14a54c44b0684aab50f7dbc5a04af64bab751
muslima <muslima241105@gmail.com>
2023-11-24 11:36:39 +0500

    Muslima, api is added

--- bin/mock_api_project3.dart
+++ bin/mock_api_project3.dart
@@ -1,3 +1,10 @@
-void main(List<String> arguments) {
+import 'package:http/http.dart';
+
+Future<void> main(List<String> arguments) async {
   print('API Project #3');
+
+  //https://655deeb89f1e1093c59a2ed1.mockapi.io/shopping
+  var url  = Uri.https('655deeb89f1e1093c59a2ed1.mockapi.io','/shopping');
+  Response response = await get(url);
+  print(response.body);
 }
